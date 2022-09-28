19fda520fada9501c0b99c04a65a9c6288693a2b
yuan-fang-228 <fy0228@gmail.com>
2022-09-27 21:01:46 -0700

    task 1

---
+++ 0x01-challenge/square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Define class Square"""
+class Square():
+    """A class of Square that calculate the areaand permiter of Square"""
+    width = 0
+    height = 0
+
+    
+    def __init__(self, *args, **kwargs):
+        """ initialise a Square class """
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def area_of_my_square(self):
+        """ Area of the square """
+        return self.width * self.width
+
+    def PermiterOfMySquare(self):
+        """ Permiter of the square """
+        return (self.width * 2) + (self.height * 2)
+
+    def __str__(self):
+        """ Reperense the square by defined string """
+        return "{}/{}".format(self.width, self.height)
+
+if __name__ == "__main__":
+
+    s = square(width=12, height=9)
+    print(s)
+    print(s.area_of_my_square())
+    print(s.PermiterOfMySquare())
